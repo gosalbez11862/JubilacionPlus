076bfd57729a45fc7d5004c1563a527058981948
gosalbez11862 <gosalbez11862@iesmarenostrum.com>
2017-02-13 20:08:18 +0100

    primera subida

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Trabajador;
+
+
+namespace Trabajador
+{
+    public partial class Trabajador : Form
+    {
+        public Trabajador()
+        {
+            InitializeComponent();
+        }
+        Trabajador PrimerTrabajador = new Trabajador();
+
+        private void BCrear_Click(object sender, EventArgs e)
+        {
+            PrimerTrabajador.Nombre = TName.Text;
+            PrimerTrabajador.Edad = int.Parse(TEdad.Text);
+        }
+
+        private void BJubilacion_Click(object sender, EventArgs e)
+        {
+            string DatosTrabajador = FNombreEdadJubliacion(PrimerTrabajador.Nombre, PrimerTrabajador.Edad, PrimerTrabajador.CalculoAnyosJubilacion());
+        }
+
+        string FNombreEdadJubliacion(string nombre, int edad, int jubilacion)
+        {
+            string DatosTrabajador ="";
+
+            DatosTrabajador += "Nombre: " + nombre + "\n\n";
+            DatosTrabajador += "Edad: " + edad + "\n\n";
+            DatosTrabajador += "Años para jubilarse: " + jubilacion + "\n\n";
+
+            return DatosTrabajador;
+        }
+    }
+}
